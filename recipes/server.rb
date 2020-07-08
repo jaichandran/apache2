f5781b12e9e7d3f46f48b173ffd4c1ed7ff68ab7
Jaichandran <jai2kus@gmail.com>
2020-07-08 15:24:33 +0000

    initial cookbook code

---
+++ recipes/server.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: apache
+# Recipe:: server
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+#
+package "apache2" do 
+	action :install
+end
+file "/var/www/html/index.html" do
+	content "<h1> Hello from apache cookbook </h1>"
+end
+service "apache2" do
+	action [:enable, :start]
+end	
